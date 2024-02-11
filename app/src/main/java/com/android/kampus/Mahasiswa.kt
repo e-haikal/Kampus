0d51ca52570ce3269845215963c8b51bd82fd783
Asman Haikal <asmanhaikal@gmail.com>
2024-02-11 23:56:28 +0700

    recycler view update 1

--- app/src/main/java/com/android/kampus/Mahasiswa.kt
+++ app/src/main/java/com/android/kampus/Mahasiswa.kt
@@ -3,6 +3,7 @@ package com.android.kampus
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,17 +15,26 @@ class Mahasiswa : AppCompatActivity() {
         val rv_mahasiswa:RecyclerView = findViewById(R.id.rv_mahasiswa)
 
         val nama:MutableList<String> = mutableListOf()
-        nama.add("Haikal")
-        nama.add("Arif")
+        nama.add("Chintia")
         nama.add("Akira")
+        nama.add("Laras")
+        nama.add("Bilqis")
+
         val nim:MutableList<String> = mutableListOf()
         nim.add("23.22.0001")
         nim.add("23.22.0002")
         nim.add("23.22.0003")
+        nim.add("23.22.0004")
 
-        val mi = Mahasiswa_Item(nama, nim)
+        val foto:MutableList<Int> = mutableListOf()
+        foto.add(R.drawable.produk_alesha)
+        foto.add(R.drawable.produk_bergo)
+        foto.add(R.drawable.produk_pasmina)
+        foto.add(R.drawable.produk_zayana)
+
+        val mi = Mahasiswa_Item(nama, nim, foto)
 
         rv_mahasiswa.adapter = mi
-        rv_mahasiswa.layoutManager = LinearLayoutManager(this)
+        rv_mahasiswa.layoutManager = GridLayoutManager (this, 2)
     }
 }
--- app/src/main/java/com/android/kampus/Mahasiswa_Item.kt
+++ app/src/main/java/com/android/kampus/Mahasiswa_Item.kt
@@ -3,12 +3,13 @@ package com.android.kampus
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import java.time.temporal.ValueRange
 
-class Mahasiswa_Item (val nama:MutableList<String>, val nim:MutableList<String>) : RecyclerView.Adapter<Mahasiswa_Item.ViewHolder>(){
+class Mahasiswa_Item (val nama:MutableList<String>, val nim:MutableList<String>, val foto:MutableList<Int>) : RecyclerView.Adapter<Mahasiswa_Item.ViewHolder>(){
     //dapatkan dulu layoutnya mahasiswa_item
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Mahasiswa_Item.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.mahasiswa_item, parent, false)
@@ -17,13 +18,15 @@ class Mahasiswa_Item (val nama:MutableList<String>, val nim:MutableList<String>)
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView){
         val txt_nama:TextView = ItemView.findViewById(R.id.txt_nama)
         val txt_nim:TextView = ItemView.findViewById(R.id.txt_nim)
+        val iv_foto:ImageView = ItemView.findViewById(R.id.iv_foto)
     }
     override fun getItemCount(): Int {
         return nama.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.txt_nama.text = "kadal"
-        holder.txt_nim.text = "air"
+        holder.txt_nama.text = nama.get(position)
+        holder.txt_nim.text = nim.get(position)
+        holder.iv_foto.setImageResource(foto.get(position))
     }
 }
