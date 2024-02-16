d55773c396ca01d1e1f30d8137404f5b00d76920
Asman Haikal <asmanhaikal@gmail.com>
2024-02-17 01:09:17 +0700

    Initial commit

--- app/src/main/java/com/android/kampus/Mahasiswa.kt
+++ app/src/main/java/com/android/kampus/Mahasiswa.kt
@@ -1,7 +1,10 @@
 package com.android.kampus
 
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,27 +17,35 @@ class Mahasiswa : AppCompatActivity() {
 
         val rv_mahasiswa:RecyclerView = findViewById(R.id.rv_mahasiswa)
 
+        val id_mahasiswa:MutableList<String> = mutableListOf()
         val nama:MutableList<String> = mutableListOf()
-        nama.add("Chintia")
-        nama.add("Akira")
-        nama.add("Laras")
-        nama.add("Bilqis")
-
         val nim:MutableList<String> = mutableListOf()
-        nim.add("23.22.0001")
-        nim.add("23.22.0002")
-        nim.add("23.22.0003")
-        nim.add("23.22.0004")
-
         val foto:MutableList<Int> = mutableListOf()
-        foto.add(R.drawable.produk_alesha)
-        foto.add(R.drawable.produk_bergo)
-        foto.add(R.drawable.produk_pasmina)
-        foto.add(R.drawable.produk_zayana)
 
-        val mi = Mahasiswa_Item(nama, nim, foto)
+        //panggil database kampus
+        val dbkampus:SQLiteDatabase = openOrCreateDatabase("kampus", MODE_PRIVATE, null)
+
+        //gali data mahasiswa
+        val gali_mahasiswa = dbkampus.rawQuery("SELECT * FROM mahasiswa", null)
+
+        while (gali_mahasiswa.moveToNext())
+        {
+            id_mahasiswa.add(gali_mahasiswa.getString(0))
+            nama.add(gali_mahasiswa.getString(1))
+            nim.add(gali_mahasiswa.getString(2))
+            foto.add(R.drawable.noimage)
+        }
+
+        val mi = Mahasiswa_Item(this,   id_mahasiswa, nama, nim, foto)
 
         rv_mahasiswa.adapter = mi
         rv_mahasiswa.layoutManager = GridLayoutManager (this, 2)
+
+        //btn_tambah
+        val btn_tambah:Button = findViewById(R.id.btn_tambah)
+        btn_tambah.setOnClickListener {
+            val pindah:Intent = Intent(this, Mahasiswa_Tambah::class.java)
+            startActivity(pindah)
+        }
     }
 }
--- app/src/main/java/com/android/kampus/Mahasiswa_Item.kt
+++ app/src/main/java/com/android/kampus/Mahasiswa_Item.kt
@@ -1,15 +1,18 @@
 package com.android.kampus
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import java.time.temporal.ValueRange
 
-class Mahasiswa_Item (val nama:MutableList<String>, val nim:MutableList<String>, val foto:MutableList<Int>) : RecyclerView.Adapter<Mahasiswa_Item.ViewHolder>(){
+class Mahasiswa_Item (val ini:Context, val id_mahasiswa:MutableList<String>, val nama:MutableList<String>, val nim:MutableList<String>, val foto:MutableList<Int>) : RecyclerView.Adapter<Mahasiswa_Item.ViewHolder>(){
     //dapatkan dulu layoutnya mahasiswa_item
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Mahasiswa_Item.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.mahasiswa_item, parent, false)
@@ -19,6 +22,7 @@ class Mahasiswa_Item (val nama:MutableList<String>, val nim:MutableList<String>,
         val txt_nama:TextView = ItemView.findViewById(R.id.txt_nama)
         val txt_nim:TextView = ItemView.findViewById(R.id.txt_nim)
         val iv_foto:ImageView = ItemView.findViewById(R.id.iv_foto)
+        val btn_hapus:Button = ItemView.findViewById(R.id.btn_hapus)
     }
     override fun getItemCount(): Int {
         return nama.size
@@ -28,5 +32,16 @@ class Mahasiswa_Item (val nama:MutableList<String>, val nim:MutableList<String>,
         holder.txt_nama.text = nama.get(position)
         holder.txt_nim.text = nim.get(position)
         holder.iv_foto.setImageResource(foto.get(position))
+
+        //btn_hapus ditekan
+        holder.btn_hapus.setOnClickListener {
+            //dapatkan id_mahasiswa sesuai urutan tombol yang ditekan
+            val id_mahasiswa_terpilih:String = id_mahasiswa.get(position)
+
+            //larikan ke activity mahasiswa_hapus
+            val pindah:Intent = Intent(ini, Mahasiswa_Hapus::class.java)
+            pindah.putExtra("id_mahasiswa_terpilih", id_mahasiswa_terpilih)
+            ini.startActivity(pindah)
+        }
     }
 }
